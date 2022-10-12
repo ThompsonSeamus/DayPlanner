05045f94a618ed7d6cad32d2e22896e9bce04a9e
seamu.th2363 <seamu.th2363@vusd.us>
2022-10-12 15:28:04 -0700

    Tried firebase, failed TODO everything

--- app/src/main/java/com/example/dayplanner/MainActivity.java
+++ app/src/main/java/com/example/dayplanner/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.dayplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Event> events = new ArrayList<Event>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToAddEvent(View view){
+        Intent intent = new Intent(MainActivity.this, AddEventActivity.class);
+        startActivity(intent);
+    }
+
+    public void addEvent(Event event){
+
     }
 }
